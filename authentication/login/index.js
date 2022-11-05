5c600c88ff35f0a3e7bb4d2bf56ec4858b012e6a
descallar24 <ralphronan5@gmail.com>
2022-11-05 23:45:28 +0800

    okay na ang login w/ css ang eye icon for hide and show nlng

--- authentication/login/index.js
+++ authentication/login/index.js
@@ -13,22 +13,24 @@ export default function Login (){
     return (
         <ImageBackground source={require('./309801225_1271235570111784_2236775530307066990_n.png')} resizeMode = "cover" style = {styles.bgimage}>
         <View  style ={styles.container}>
+            <Image source={require('./electriCAL__2_-removebg-preview.png')}style = {styles.image}/>
             <Text>{warning}</Text>
             <TextInput style={styles.txtinput} placeholder="Email Address" value={email} onChangeText={setEmail}/>
             <TextInput style={styles.txtinput} placeholder="Password"  value={password} onChangeText={setPassword} secureTextEntry={true}/>
-            <TouchableOpacity style={styles.loginButton} >
-                <Button title ="Log in" onPress={() =>{
+            <TouchableOpacity style={styles.loginButton} onPress={() =>{
                     navigation.navigate('Dashboard')
-                }}/>
+                }}>
+                <Text style={styles.text}>LOG IN</Text>    
             </TouchableOpacity>
-            <TouchableOpacity style={styles.Button}>
-                <Button title ="Create Account" onPress={() =>{
+            <TouchableOpacity onPress={() =>{
                     navigation.navigate('Registration');
-                }} />
+                }}>
+                <Text style={styles.fg}>Forgot Password?</Text>
               </TouchableOpacity>
-            <Text onPress={()=> {
-                navigation.navigate('Registraton')
-            }}>Forgot password?</Text>
+            <Text style={styles.fg}>Don't have an account?
+                 <Text onPress={() =>{
+                    navigation.navigate('Registration');
+                }} style={styles.fg2}> Sign Up </Text> </Text>
         </View>
         </ImageBackground>
     )
@@ -44,28 +46,70 @@ const styles = StyleSheet.create({
     
     txtinput:{
         borderWidth: 2,
-        height: 40,
+        height: 50,
         width: 300,
         padding:10,
         margin:5,
+        marginTop: 10,
         textAlign:'center',
-        borderRadius: 20
+        borderRadius: 20,
+        backgroundColor: '#EBE8CD',
+        marginBottom: 10,
+        justifyContent: 'center',
+        fontFamily: "sans-serif-condensed"
+
     },
     
-    Button: {
+    fg: {
+        width:250,
+        back: '#463a0b',
+        textAlign: "center",
+        justifyContent: 'center',
+        marginBottom: 5,
+        fontFamily: "sans-serif-condensed",
+        fontSize: 15,
+
+
+    },
+
+    fg2: {
         margin: 5,
         width:200,
-        back: '#463a0b'
+        back: '#463a0b',
+        textAlign: "center",
+        color: "#3F52FD",
+        fontWeight: "800",
+        textDecorationLine: 'underline',
+        fontFamily: "sans-serif-condensed"
     },
 
     loginButton: {
         width: 200,
-        backgroundColor: '#DCB900'
+        backgroundColor: '#DCB900',
+        height: 50,
+        width: 250,
+        borderRadius: 20,
+        textAlign:'center',
+        marginBottom: 5,
+    },
+
+    text:{
+        fontStyle: "normal",
+        fontSize: 25,
+        textAlign:'center',
+        height: 60,
+        padding:5,
+        margin:5,
+        fontWeight: "bold",
+        fontFamily: "sans-serif-condensed"
+
     },
     image:{
         justifyContent: "center",
-        width: 250,
-        height: 250
+        width: 300,
+        height: 280,
+        marginBottom: -20
+    
     },
     bgimage: {
         flex: 1
