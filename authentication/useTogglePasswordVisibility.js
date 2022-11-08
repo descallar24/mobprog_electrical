e24d2d9f59548dfecd1330a9fb7155d3dcceb809
descallar24 <ralphronan5@gmail.com>
2022-11-08 13:27:01 +0800

    latest

--- authentication/useTogglePasswordVisibility.js
+++ authentication/useTogglePasswordVisibility.js
@@ -2,9 +2,7 @@ import { useState } from "react";
 
 export const useTogglePasswordVisibility = () => {
     const [passwordVisibility, setPasswordVisibility] = useState(true);
-    const [passwordVisibility2, setPasswordVisibility2] = useState(true);
     const [rightIcon, setRightIcon] = useState('eye');
-    const [rightIcon2, setRightIcon2] = useState('eye');
   
     const handlePasswordVisibility = () => {
       if (rightIcon === 'eye') {
